f0e4723f1d02958d4040a1b56627fb7d198ac31d
shawnpchampion <shawnpchampion@gmail.com>
2021-10-13 17:01:04 -1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,6 +1,6 @@
 // paste in your published Google Sheets URL from the browser address bar
 // var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HzZTBN7dC_is6ut2tj_E3o-xbTxAu8rLrc5q7lzfDz0/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1oLRSMJU_Ak0E-JZ3bW4EBJD2x6Ke9SmRpi5l6MUM98I/edit#gid=0';
 
 
 // insert your own Google Sheets API key from https://console.developers.google.com
