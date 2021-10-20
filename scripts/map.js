99be657c9d0ed6544123237abc4b4f246b7236f3
shawnpchampion <shawnpchampion@gmail.com>
2021-10-20 03:55:29 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -130,10 +130,10 @@ $(window).on('load', function() {
         
       if (point.Latitude !== '' && point.Longitude !== '') {
         
-        var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description']);
+        var marker = L.marker([point.Latitude, point.Longitude], {icon: icon});
+    //      .bindPopup("<b>" + point['Name'] + '</b><br>' +
+    //      (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
+    //      point['Description']);
        
    
         
