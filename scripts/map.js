f02fdf1fada4f0d9450ccc7d9af7bcf6f95ebedf
shawnpchampion <shawnpchampion@gmail.com>
2021-10-20 05:30:08 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -143,9 +143,10 @@ $("#featureModal").modal({show:false});
         markerArray.push(marker);  
         
         for (var i in markerArray){
-        var markerID = point['Name'];
-        markerArray[i].openPopup();
+  //      var markerID = point['Name'];
+  //      markerArray[i].openPopup();
         markerArray[i].on('click', function() {
+            $("#feature-title").html([point'Name']);
             $("#featureModal").modal("show"); 
         });
     };
