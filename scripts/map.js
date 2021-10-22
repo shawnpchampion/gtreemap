594ea6e67192da99085df1a2039adb841513b729
shawnpchampion <shawnpchampion@gmail.com>
2021-10-21 14:36:36 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -24,14 +24,14 @@ $(window).on('load', function() {
 
   
 $("#full-extent-btn").click(function() {
-  $("#aboutModal").modal("show");
-  $(".navbar-collapse.in").collapse("hide");
-  return false;
-//  map.fitBounds(boroughs.getBounds());
-//  map.fitBounds(markers.getBounds().pad(0.5))
-//  baseLayers["Satellite Map"].addTo(map);
+//  $("#aboutModal").modal("show");
 //  $(".navbar-collapse.in").collapse("hide");
 //  return false;
+//    layerControl.addBaseLayer(newBaseLayer, 'My New BaseLayer');
+  $('.leaflet-control-layers-selector')[0];
+//  baseLayers["Satellite Map"].addTo(map);
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
 });
   
   
