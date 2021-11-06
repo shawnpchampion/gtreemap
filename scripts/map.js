af20278712309fd93910a62aeb5a6aa8ac08c2cf
shawnpchampion <shawnpchampion@gmail.com>
2021-11-06 04:32:47 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -1186,7 +1186,7 @@ var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
   });
 
 var cartoLight = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-  maxZoom: 19,
+  maxZoom: 20,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
   });
 
