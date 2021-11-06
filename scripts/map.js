9ac595506febe260e1843a0bf1dd89da94b95b64
shawnpchampion <shawnpchampion@gmail.com>
2021-11-06 09:58:02 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -23,47 +23,6 @@ $(window).on('load', function() {
     });
   }
 
- 
-  
-/**
- * Sets the map view so that all markers are visible, or
- * to specified (lat, lon) and zoom if all three are specified
- */
-//  function centerAndZoomMap(points) {
-//    var lat = map.getCenter().lat, latSet = false;
-//    var lon = map.getCenter().lng, lonSet = false;
-//    var zoom = 12, zoomSet = false;
-//    var center;
-
-//    if (getSetting('_initLat') !== '') {
-//      lat = getSetting('_initLat');
-//      latSet = true;
-//    }
-
-//    if (getSetting('_initLon') !== '') {
-//      lon = getSetting('_initLon');
-//      lonSet = true;
-//    }
-
-//    if (getSetting('_initZoom') !== '') {
-//      zoom = parseInt(getSetting('_initZoom'));
-//      zoomSet = true;
-//    }
-
-//    if ((latSet && lonSet) || !points) {
-//      center = L.latLng(lat, lon);
-//    } else {
-//      center = points.getBounds().getCenter();
-//    }
-
-//    if (!zoomSet && points) {
-//      zoom = map.getBoundsZoom(points.getBounds());
-//    }
-
-//   map.setView(center, zoom);
-	  
-//  }
-
 
 /**
  * Given a collection of points, determines the layers based on 'Group'
@@ -677,8 +636,8 @@ $(window).on('load', function() {
       completePoints = true;
     }
     
-    map.setView([19.408548, -154.914111],14);	  
-//    centerAndZoomMap(group);
+    map.setView([19.408548, -154.914111],17);	  
+
 
 // Add polylines
     if (polylines && polylines.length > 0) {
