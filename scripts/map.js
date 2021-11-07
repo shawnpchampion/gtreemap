c0a3a4d459a3f0b33c8f255c2cadaf6c55ee73bd
shawnpchampion <shawnpchampion@gmail.com>
2021-11-06 17:33:32 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -125,7 +125,7 @@ $(window).on('load', function() {
       
       if (getSetting('_pointsLegendPos') !== 'off') {
         pointsLegend.addTo(map);
-//        pointsLegend._container.id = 'points-legend';
+        pointsLegend._container.id = 'points-legend';
 //        pointsLegend._container.className += ' ladder';
       }
     }
