68bb3abb28299ae88ad9be990b1e55c3ecd55834
shawnpchampion <shawnpchampion@gmail.com>
2021-11-06 17:26:56 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -125,17 +125,17 @@ $(window).on('load', function() {
       
       if (getSetting('_pointsLegendPos') !== 'off') {
         pointsLegend.addTo(map);
-        pointsLegend._container.id = 'points-legend';
+//        pointsLegend._container.id = 'points-legend';
 //        pointsLegend._container.className += ' ladder';
       }
     }
         
-    $('#points-legend').prepend('<h6 class="pointer"><b>' + getSetting('_pointsLegendTitle') + '</b></h6>');
+//    $('#points-legend').prepend('<h6 class="pointer"><b>' + getSetting('_pointsLegendTitle') + '</b></h6>');
 //    $(".leaflet-control-layers-overlays").prepend("<label><b>Trees of Interest</b></label>");
-    if (getSetting('_pointsLegendIcon') != '') {
-      $('#points-legend h6').prepend('<span class="legend-icon"><i class="fas '
-        + getSetting('_pointsLegendIcon') + '"></i></span>');
-    }
+//    if (getSetting('_pointsLegendIcon') != '') {
+//      $('#points-legend h6').prepend('<span class="legend-icon"><i class="fas '
+//        + getSetting('_pointsLegendIcon') + '"></i></span>');
+//    }
         
 // END LEGEND CODE
 
