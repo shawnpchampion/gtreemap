4a8463d91e49822118ff514d9a8ad5727ed5403d
shawnpchampion <shawnpchampion@gmail.com>
2021-11-10 14:43:15 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -76,7 +76,9 @@ $(window).on('load', function() {
       if (point.Latitude !== '' && point.Longitude !== '') {
      
 // DEFINE THE PARAMETERS OF THE MARKER, AND ADD IT TO THE MAP        
-        var marker = L.marker([point.Latitude, point.Longitude], {name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant'], icon: icon})
+//        var marker = L.marker([point.Latitude, point.Longitude], {name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant'], icon: icon})
+        var marker = L.marker([point.Latitude, point.Longitude], { tags: ["point['CPlant']"] })
+									
 	.on('click', markerOnClick)  
         .addTo(map);
         
@@ -170,7 +172,7 @@ $(window).on('load', function() {
 	  
 	  
 var tagbutton =	L.control.tagFilterButton({
-        data: ['Now', 'How'],
+        data: ["Yes", "No"],
         icon: '<i class="fa fa-suitcase"></i>',
         filterOnEveryClick: true,
         }).addTo( map );	  
