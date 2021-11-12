4835898ec4cdd23afcd2fa4bc8a72915c8b8c158
shawnpchampion <shawnpchampion@gmail.com>
2021-11-11 20:45:37 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -83,6 +83,14 @@ $(window).on('load', function() {
 	var marker = L.circlemarker([point.Latitude, point.Longitude], {radius: 5, color: '#FF0000', name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant']})									  
 	.on('click', markerOnClick)  
         .addTo(map);
+	      
+	          circle2 = L.circleMarker([5,8]  ).addTo(map);
+    
+    circle = L.circleMarker([2,1] ).addTo(map);
+ 
+circle.setStyle({color: 'green'});
+
+circle2.setStyle({color: 'red'});
         
 // DEFINE THE FEATURES FOR THE MODAL POPUP	
         function markerOnClick(e)
