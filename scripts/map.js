ead41b01f141c6568ecdd2f360c9c974e21c8952
shawnpchampion <shawnpchampion@gmail.com>
2021-11-11 20:46:03 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -78,9 +78,9 @@ $(window).on('load', function() {
      
 // DEFINE THE PARAMETERS OF THE MARKER, AND ADD IT TO THE MAP 
 	     
-//        var marker = L.marker([point.Latitude, point.Longitude], {name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant'], icon: icon})									
+        var marker = L.marker([point.Latitude, point.Longitude], {name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant'], icon: icon})									
        
-	var marker = L.circlemarker([point.Latitude, point.Longitude], {radius: 5, color: '#FF0000', name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant']})									  
+//	var marker = L.circlemarker([point.Latitude, point.Longitude], {radius: 5, color: '#FF0000', name: point['Name'], group: point['Group'], descript: point['Description'], bimage: point['Image'], harvest: point['Harvest'], hname: point['HName'], tags: point['CPlant']})									  
 	.on('click', markerOnClick)  
         .addTo(map);
 	      
