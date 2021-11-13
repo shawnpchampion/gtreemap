3dfcddb4ca2f7391912038852ac89269a8c55db7
shawnpchampion <shawnpchampion@gmail.com>
2021-11-13 10:48:56 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -134,7 +134,7 @@ $(window).on('load', function() {
 	    
 //      var pointsLegend = L.control.layers(baseMaps, layers, {    
       var pointsLegend = L.control.layers(null, layers, { 
-	collapsed: true,      
+	collapsed: false,      
         position: 'topright',
       });
       
