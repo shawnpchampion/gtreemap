f956e0ae1ae92f94a8f5619df8f3fda7cf18316f
shawnpchampion <shawnpchampion@gmail.com>
2021-11-13 00:32:36 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -247,9 +247,9 @@ $(window).on('load', function() {
 
 // BEGIN GOOGLE SHEET CODE
   
-  function onMapDataLoad(points) {
+//  function onMapDataLoad(points) {
       
-//  function onMapDataLoad(options, points) { 
+  function onMapDataLoad(options, points) { 
       
     document.title = 'Kalani Tree Map';	  
  
@@ -342,13 +342,13 @@ $(window).on('load', function() {
               var sheets = data.sheets.map(function(o) { return o.properties.title })
                                  
               $.when(
-//                $.getJSON(apiUrl + spreadsheetId + '/values/Options?key=' + googleApiKey),
+                $.getJSON(apiUrl + spreadsheetId + '/values/Options?key=' + googleApiKey),
                 $.getJSON(apiUrl + spreadsheetId + '/values/Points?key=' + googleApiKey)
                 
-//              ).done(function(options, points) {
-	      ).done(function(points) {                    	
+              ).done(function(options, points) {
+//	      ).done(function(points) {                    	
                 onMapDataLoad(
-//                  parse(options),
+                  parse(options),
                   parse(points)
                 )
               })
