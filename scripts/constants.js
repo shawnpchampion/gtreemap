00a3b2fcda5cd67286b34325a17f361372a2461b
shawnpchampion <shawnpchampion@gmail.com>
2021-11-12 22:48:50 -1000

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,19 +1,6 @@
 var constants = {
 // Tabs in Google Sheet
-  notesSheetName: 'Notes',
-  optionsSheetName: 'Options',
+//  notesSheetName: 'Notes',
+//  optionsSheetName: 'Options',
   pointsSheetName: 'Points',
-// Map Info
-  _mapTitle: 'Map Title',
-// Map Settings
-  _markercluster: 'Cluster Markers',
-// Map Controls
-	_mapMyLocation: 'Show My Location',
-// Point Map Display
-	_pointsLegendPos: 'Point Legend Position',
-// Table Map
-  _displayTable: 'Display Table',
-  _tableHeight: 'Table Height',
-  _tableColumns: 'Table Columns',
-  _tableHeaderColor: 'Table Header Color',
 };
