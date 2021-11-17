9fceb236cbab5177017e4ccf1e175e5ae7c4db36
shawnpchampion <shawnpchampion@gmail.com>
2021-11-16 22:17:38 -1000

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -23,7 +23,7 @@ $(window).on('load', function() {
     position: 'topright',
     states: [{
       stateName: 'add-gmap',
-      icon: 'fa-grav',
+      icon: 'fab fa-grav',
       title: 'add gmap',
       onClick: function(control) {
         map.addLayer(googleSat);
